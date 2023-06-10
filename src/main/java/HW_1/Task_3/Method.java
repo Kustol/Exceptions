4afaa590c77a05e50bfea60ff2a6c5128a113dcb
Kustol <112040133+Kustol@users.noreply.github.com>
2023-06-10 20:53:11 +0800

    HW_1
    HW_1

--- src/main/java/HW_1/Task_3/Method.java
+++ src/main/java/HW_1/Task_3/Method.java
@@ -5,14 +5,14 @@ public class Method {
     public int[] methodExceptionSumArray(int[] arr1, int[] arr2) {
         int[] newArr1 = new int[] { 0 };
         if (arr1.length != arr2.length) {
-            System.out.println("Действие не может быть выполнено, \nиз-за разной длины массивов!");
+            System.out.println("The action cannot be performed, \ndue to the different length of the arrays!");
             return newArr1;
         }
         int[] newArr2 = new int[arr1.length];
         for (int i = 0; i < arr1.length; i++) {
             newArr2[i] = arr1[i] + arr2[i];
         }
-        System.out.println("Операция выполнена успешно!");
+        System.out.println("The operation was completed successfully!");
         return newArr2;
     }
 }
